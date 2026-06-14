3dbd1b949d08592713229305c5d687b20028cdf8
somya gupta <bt23ece121@iiitn.ac.in>
2026-06-14 19:17:24 +0530

    u

--- channel-service/services/emailService.js
+++ channel-service/services/emailService.js
@@ -4,19 +4,15 @@ const nodemailer =
 const transporter =
   nodemailer.createTransport({
 
-    host: "smtp.gmail.com",
+    host: "smtp-relay.brevo.com",
 
     port: 587,
 
     secure: false,
 
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-
-    tls: {
-      rejectUnauthorized: false,
+      user: process.env.BREVO_USER,
+      pass: process.env.BREVO_PASS,
     },
 
   });
