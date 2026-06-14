24722a8f1bcd05d5fdd4942f8ac034d87dc9a74e
somya gupta <bt23ece121@iiitn.ac.in>
2026-06-14 16:05:13 +0530

    done for the day

--- services/emailService.js
+++ services/emailService.js
@@ -1,45 +1,11 @@
-const nodemailer =
-  require("nodemailer");
-
-const transporter =
-  nodemailer.createTransport({
-
-    host: "smtp.gmail.com",
-
-    port: 465,
-
-    secure: true,
-
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-
-  });
-
 const sendOTPEmail = async (email, otp) => {
-  try {
 
-    console.log("Sending OTP to:", email);
-    console.log("OTP:", otp);
+  console.log("EMAIL DISABLED FOR TEST");
+  console.log("Email:", email);
+  console.log("OTP:", otp);
 
-    const info = await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: "Xeno CRM Verification OTP",
-      html: `<h1>${otp}</h1>`
-    });
+  return true;
 
-    console.log("EMAIL SENT");
-    console.log(info);
-
-  } catch (error) {
-
-    console.log("EMAIL ERROR");
-    console.log(error);
-
-    throw error;
-  }
 };
 
 module.exports = {
