3de1ba505900cade3a6dd279896e7571080b54eb
somya gupta <bt23ece121@iiitn.ac.in>
2026-06-14 15:30:18 +0530

    fix SMTP

--- channel-service/services/emailService.js
+++ channel-service/services/emailService.js
@@ -4,20 +4,39 @@ const nodemailer =
 const transporter =
   nodemailer.createTransport({
 
-    service: "gmail",
+    host: "smtp.gmail.com",
+
+    port: 587,
+
+    secure: false,
 
     auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
 
-      user:
-        process.env.EMAIL_USER,
-
-      pass:
-        process.env.EMAIL_PASS,
-
+    tls: {
+      rejectUnauthorized: false,
     },
 
   });
 
+  transporter.verify(function (error, success) {
+
+  if (error) {
+
+    console.log("SMTP ERROR:");
+    console.log(error);
+
+  } else {
+
+    console.log(
+      "SMTP SERVER READY"
+    );
+
+  }
+
+});
 const sendEmail =
   async (
     to,
